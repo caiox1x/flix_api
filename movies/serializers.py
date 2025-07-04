f07710c451c665f6cc1c6fa77f350299b09a1724
caiox1x <caiodocarmo00@gmail.com>
2025-07-03 21:54:14 -0400

    Feature de status no endpoint /movies/stats

--- movies/serializers.py
+++ movies/serializers.py
@@ -16,22 +16,6 @@ class MovieSerializer(serializers.ModelSerializer):
             return round(rate, 1)
         
         return None
-        
-        # reviews = obj.reviews.all()
-
-        # if reviews:
-        #     sum_reviews = 0
-
-        #     for review in reviews:
-        #         sum_reviews += review.stars
-
-        #     print(sum_reviews)
-
-        #     reviews_count = reviews.count()
-
-        #     return round(sum_reviews / reviews_count, 1)
-
-        # return None
     
     def validate_release_date(self, value):
         if value.year < 1900:
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
 from movies.serializers import MovieSerializer
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
@@ -13,3 +15,36 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+        movies_with_avg_stars = self.queryset.annotate(
+            avg_stars=Avg('reviews__stars'), total_reviews=Count('reviews')).values(
+            'id', 'title', 'avg_stars', 'total_reviews'
+        )
+
+        return response.Response(
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_reviews': total_reviews,
+                'average_stars': round(average_stars, 1) if average_stars else 0,
+                'average_stars_per_movie': [
+                    {
+                        'id': movie['id'],
+                        'title': movie['title'],
+                        'average_stars': round(movie['avg_stars'], 1) if movie['avg_stars'] is not None else 0,
+                        'total_reviews': movie['total_reviews']
+                    }
+                    for movie in movies_with_avg_stars
+                ]
+            },
+            status=status.HTTP_200_OK,
+        )
